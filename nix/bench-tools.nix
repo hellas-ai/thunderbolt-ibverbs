7de20c101af7ecbf090eff7e5fcaf596af17e4f0
georgewhewell <georgerw@gmail.com>
2026-05-28 10:52:02 +0200

    nix: switch darwin perftest/bench-tools to apple-sdk_26

--- nix/bench-tools.nix
+++ nix/bench-tools.nix
@@ -1,11 +1,11 @@
 { lib
 , stdenv
 , pkg-config
+, apple-sdk_26 ? null
 , rdma-core-usb4 ? null
 , python3
 , source ? ../userspace/bench
 , appleCompat ? ./apple-compat
-, appleRdmaSdk ? null
 }:
 
 let
@@ -34,8 +34,8 @@ let
 
   cPrograms = if isDarwin then darwinPrograms else linuxPrograms;
 in
-assert lib.assertMsg (!isDarwin || appleRdmaSdk != null)
-  "bench-tools Darwin build requires appleRdmaSdk";
+assert lib.assertMsg (!isDarwin || apple-sdk_26 != null)
+  "bench-tools Darwin build requires apple-sdk_26";
 stdenv.mkDerivation {
   pname =
     if isDarwin
@@ -55,7 +55,9 @@ stdenv.mkDerivation {
   };
 
   nativeBuildInputs = lib.optionals (!isDarwin) [ pkg-config ];
-  buildInputs = lib.optionals (!isDarwin) [ rdma-core-usb4 ] ++ [ python3 ];
+  buildInputs = lib.optionals (!isDarwin) [ rdma-core-usb4 ]
+    ++ lib.optionals isDarwin [ apple-sdk_26 ]
+    ++ [ python3 ];
 
   dontConfigure = true;
 
@@ -65,9 +67,8 @@ stdenv.mkDerivation {
       for name in ${lib.concatStringsSep " " cPrograms}; do
         $CC -O2 -Wall -Wextra -std=gnu11 \
           -I${appleCompat} \
-          -I${appleRdmaSdk}/include \
           "$name.c" \
-          -Wl,-undefined,dynamic_lookup \
+          -lrdma \
           -o "$name"
       done
       runHook postBuild
